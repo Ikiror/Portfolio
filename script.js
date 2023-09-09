63a2f67e3b25130d050e0181fd121c308ac09e11
Amo Ikiror <amoikiror@gmail.com>
2023-09-08 22:18:16 -0400

    work on adding button, JS, .map functionality

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
+// list of skills
 const skills = [
-    { 'HTML', 'Bootstrap', 'CSS', 'SASS/SCSS', 'Github', 'NPM', 'Wireframe', 'JavaScript'
-}
+    'HTML', 'Bootstrap', 'CSS', 'SASS/SCSS', 'Github', 'NPM', 'Wireframe', 'JavaScript', "and I'm constantly learning new things:)"
 ]
 
-function display(skill) {
-    return 'My Skills: ' + skill ;
-}
+let i=0; 
+function addSkill() {
+    function revealSkill(skill) {
+        return skill ;
+    }
+    const learnedSkills = skills.map(revealSkill);
+  
+    const newListItem = document.createElement("li");
+    for (i < learnedSkills.length; i++;) {
+        // newListItem.innerHTML = learnedSkills[i]
+        const newSkill = document.createTextNode(learnedSkills[i]);
+        newListItem.append(newSkill);
+        const unorderedList = document.getElementById("skillTxt");
+    
+    document.body.after(newListItem, unorderedList);
 
-const learnedSkills = skills.map(display);
+    // const skillText = document.getElementById("skillTxt");
+    }   
+};
 
-for (x in learnedSkills) {
-    document.getElementById('skillsTrigger').innerHTML
-}
+let revealButton = document.getElementById("skillBtn");
+revealButton.onclick = addSkill;
 
-<button id="getQuote" onkeypress="diplayRandomQuote()">
-        Click to get a new quote!
-      </button>
+// for (let i=0; i < learnedSkills.length; i++) {
+//     skillText.innerHTML = learnedSkills[i]
+// }
+// function addSkill () {
+//     for (x in learnedSkills) {
+//     const newListItem = document.createElement("li");
 
-let quoteButton = document.getElementById("getQuote");
-quoteButton.onclick = displayRandomQuote;
+//     // and give it some content
+    
+//     const newSkill = document.createTextNode(learnedSkills[x]);
+  
+  
+//     // add the text node to the newly created div
+//     newListItem.appendChild(newSkill);
+  
+//     // add the newly created element and its content into the DOM
+//     const unorderedList = document.getElementById("skillTxt");
+//     document.body.insertBefore(newListItem, unorderedList);
+//     }
+// }
+
+// let revealButton = document.getElementById("skillBtn");
+// revealButton.onclick = learnedSkills;
+
