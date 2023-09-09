5549b5de3ee3e53185304fbfaa44135e7a4803cc
Amo Ikiror <amoikiror@gmail.com>
2023-09-09 00:04:39 -0400

    update to button function

--- script.js
+++ script.js
@@ -3,28 +3,37 @@ const skills = [
     'HTML', 'Bootstrap', 'CSS', 'SASS/SCSS', 'Github', 'NPM', 'Wireframe', 'JavaScript', "and I'm constantly learning new things:)"
 ]
 
-let i=0; 
-function addSkill() {
-    function revealSkill(skill) {
+
+// function addSkill() {
+//     function revealSkill(skill) {
+//         return skill ;
+//     }
+//     const learnedSkills = skills.map(revealSkill);
+  
+//     const newListItem = document.createElement("li");
+//     for (let i=0; i < learnedSkills.length; i++) {
+//         // newListItem.innerHTML = learnedSkills[i]
+//         const newSkill = document.createTextNode(learnedSkills[i]);
+//         newListItem.append(newSkill);
+//         const unorderedList = document.getElementById("skillTxt");
+    
+//     document.body.insertBefore(newListItem, unorderedList);
+
+//     // const skillText = document.getElementById("skillTxt");
+//     }   
+// };
+function addSkill(skill) {
         return skill ;
     }
-    const learnedSkills = skills.map(revealSkill);
-  
-    const newListItem = document.createElement("li");
-    for (i < learnedSkills.length; i++;) {
-        // newListItem.innerHTML = learnedSkills[i]
-        const newSkill = document.createTextNode(learnedSkills[i]);
-        newListItem.append(newSkill);
-        const unorderedList = document.getElementById("skillTxt");
-    
-    document.body.after(newListItem, unorderedList);
-
-    // const skillText = document.getElementById("skillTxt");
-    }   
-};
-
+const learnedSkills = skills.map(addSkill)
+const revealSkill = () => {
+    let randomSkillNum = Math.floor(Math.random() * skills.length);
+    let skill = learnedSkills[randomSkillNum];
+    const revealedSkill = document.getElementById("skillText")
+    revealSkill.innerHTML = skill;
+}
 let revealButton = document.getElementById("skillBtn");
-revealButton.onclick = addSkill;
+revealButton.onclick = revealSkill;
 
 // for (let i=0; i < learnedSkills.length; i++) {
 //     skillText.innerHTML = learnedSkills[i]
